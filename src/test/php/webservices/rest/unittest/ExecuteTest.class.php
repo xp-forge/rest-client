fa860825b848acc9327ca60479a787308278d46a
Timm Friebe <thekid@thekid.de>
2018-11-02 23:42:54 +0100

    Compress Cookie header according to RFC 6265

--- src/test/php/webservices/rest/unittest/ExecuteTest.class.php
+++ src/test/php/webservices/rest/unittest/ExecuteTest.class.php
@@ -11,8 +11,11 @@ class ExecuteTest extends TestCase {
   public function get() {
     $fixture= (new Endpoint('http://test'))->connecting(function($uri) { return new TestConnection($uri); });
 
-    $response= $fixture->resource('/test')->get();
-    $this->assertEquals("GET /test HTTP/1.1\r\nConnection: close\r\nHost: test\r\n\r\n", $response->content());
+    $response= $fixture->resource('/test');
+    $this->assertEquals(
+      "GET /test HTTP/1.1\r\nConnection: close\r\nHost: test\r\n\r\n",
+      $response->get()->content()
+    );
   }
 
   #[@test]
@@ -27,7 +30,32 @@ class ExecuteTest extends TestCase {
   public function get_with_parameters_in_resource() {
     $fixture= (new Endpoint('http://test'))->connecting(function($uri) { return new TestConnection($uri); });
 
-    $response= $fixture->resource('/?username={0}', ['test'])->get();
-    $this->assertEquals("GET /?username=test HTTP/1.1\r\nConnection: close\r\nHost: test\r\n\r\n", $response->content());
+    $resource= $fixture->resource('/?username={0}', ['test']);
+    $this->assertEquals(
+      "GET /?username=test HTTP/1.1\r\nConnection: close\r\nHost: test\r\n\r\n",
+      $resource->get()->content()
+    );
+  }
+
+  #[@test]
+  public function get_with_cookies() {
+    $fixture= (new Endpoint('http://test'))->connecting(function($uri) { return new TestConnection($uri); });
+
+    $resource= $fixture->resource('/')->including(['session' => '0x6100', 'lang' => 'de']);
+    $this->assertEquals(
+      "GET / HTTP/1.1\r\nConnection: close\r\nHost: test\r\nCookie: session=0x6100; lang=de\r\n\r\n",
+      $resource->get()->content()
+    );
+  }
+
+  #[@test]
+  public function get_with_cookies_merges_cookie_header() {
+    $fixture= (new Endpoint('http://test'))->connecting(function($uri) { return new TestConnection($uri); });
+
+    $resource= $fixture->resource('/')->with(['Cookie' => 'session=0x6100'])->including(['lang' => 'de']);
+    $this->assertEquals(
+      "GET / HTTP/1.1\r\nConnection: close\r\nHost: test\r\nCookie: session=0x6100; lang=de\r\n\r\n",
+      $resource->get()->content()
+    );
   }
 }
