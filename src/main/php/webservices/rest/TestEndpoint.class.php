c01596242aa027626e4e67530a0086625386d30c
Timm Friebe <t.friebe@enbw.com>
2021-11-28 13:15:16 +0100

    QA: Use plural form

--- src/main/php/webservices/rest/TestEndpoint.class.php
+++ src/main/php/webservices/rest/TestEndpoint.class.php
@@ -2,7 +2,7 @@
 
 /**
  * Endpoint subclass used for testing REST clients.
- * 
+ *
  * ```php
  * $endpoint= new TestEndpoint([
  *  '/users/me' => function($call) {
@@ -10,7 +10,7 @@
  *   }
  * ]);
  * ```
- * 
+ *
  * @see   webservices.rest.TestCall
  * @test  webservices.rest.unittest.TestEndpointTest
  */
@@ -19,7 +19,7 @@ class TestEndpoint extends Endpoint {
 
   /**
    * Creates a new testing endpoint
-   * 
+   *
    * @param  [:function(webservices.rest.TestCall): webservices.rest.RestResponse]
    */
   public function __construct(array $routes) {
@@ -39,7 +39,7 @@ class TestEndpoint extends Endpoint {
       ?? $this->routes[$req->path()]
       ?? function() { return new RestResponse(404, 'No route', []); }
     ;
-    
+
     return $handler(new TestCall($req, $this->formats, $this->marshalling));
   }
 }
--- src/test/php/webservices/rest/unittest/TestEndpointTest.class.php
+++ src/test/php/webservices/rest/unittest/TestEndpointTest.class.php
@@ -13,27 +13,27 @@ class TestEndpointTest {
   #[Test]
   public function routed_path() {
     $fixture= new TestEndpoint([
-      '/user/self' => function($call) {
-        return $call->respond(307, 'Temporary Redirect', ['Location' => '/user/6100']);
+      '/users/self' => function($call) {
+        return $call->respond(307, 'Temporary Redirect', ['Location' => '/users/6100']);
       }
     ]);
-    $r= $fixture->resource('/user/self')->get();
+    $r= $fixture->resource('/users/self')->get();
 
     Assert::equals(307, $r->status());
-    Assert::equals('/user/6100', $r->header('Location'));
+    Assert::equals('/users/6100', $r->header('Location'));
   }
 
   #[Test]
   public function routed_path_with_content() {
     $fixture= new TestEndpoint([
-      '/user/6100' => function($call) {
+      '/users/6100' => function($call) {
         return $call->respond(200, 'OK', ['Content-Type' => 'application/json'], '{
           "id": 6100,
           "handle": "test"
         }');
       }
     ]);
-    $r= $fixture->resource('/user/6100')->get();
+    $r= $fixture->resource('/users/6100')->get();
 
     Assert::equals(200, $r->status());
     Assert::equals(['id' => 6100, 'handle' => 'test'], $r->value());
@@ -42,11 +42,11 @@ class TestEndpointTest {
   #[Test]
   public function routed_path_with_method() {
     $fixture= new TestEndpoint([
-      'DELETE /user/1' => function($call) {
+      'DELETE /users/1' => function($call) {
         return $call->respond(204, 'No content');
       }
     ]);
-    $r= $fixture->resource('/user/1')->delete();
+    $r= $fixture->resource('/users/1')->delete();
 
     Assert::equals(204, $r->status());
     Assert::equals('', $r->content());
