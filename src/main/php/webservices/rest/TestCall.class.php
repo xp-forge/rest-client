5e6f61197167e530775ba4a31fe5a0e4c668de93
Timm Friebe <thekid@thekid.de>
2021-11-30 19:06:16 +0100

    Fix PHP 8.1 compatibility

--- src/main/php/webservices/rest/TestCall.class.php
+++ src/main/php/webservices/rest/TestCall.class.php
@@ -1,8 +1,8 @@
 <?php namespace webservices\rest;
 
-use webservices\rest\io\{Reader, Transmission};
 use io\streams\{InputStream, MemoryInputStream};
 use util\data\Marshalling;
+use webservices\rest\io\{Reader, Transmission};
 
 class TestCall extends Transmission {
   private $formats, $marshalling;
@@ -48,7 +48,7 @@ class TestCall extends Transmission {
    * @param  string|io.streams.InputStream $payload
    * @return webservices.rest.RestResponse
    */
-  public function respond($status, $message, $headers= [], $payload= null): RestResponse {
+  public function respond($status, $message, $headers= [], $payload= ''): RestResponse {
     return new RestResponse($status, $message, $headers, new Reader(
       $payload instanceof InputStream ? $payload : new MemoryInputStream($payload),
       $this->formats->named($headers['Content-Type'] ?? null),
