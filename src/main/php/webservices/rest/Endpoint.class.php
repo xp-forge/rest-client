eefb9fd9e7b1d9b14f1909aa72ce8c6f649545df
Timm Friebe <thekid@thekid.de>
2022-03-08 23:28:11 +0100

    Fix missing port

--- src/main/php/webservices/rest/Endpoint.class.php
+++ src/main/php/webservices/rest/Endpoint.class.php
@@ -3,7 +3,7 @@
 use lang\{Throwable, IllegalArgumentException};
 use peer\URL;
 use peer\http\{HttpConnection, HttpRequest};
-use util\URI;
+use util\{URI, Authority};
 use util\data\Marshalling;
 use util\log\Traceable;
 use webservices\rest\io\{Buffered, Reader, Streamed, Traced, Transmission};
@@ -52,7 +52,12 @@ class Endpoint implements Traceable {
       $this->headers['Authorization']= 'Bearer '.$user;
     }
 
-    $this->base= $uri->using()->authority($authority->host())->path(rtrim($uri->path() ?? '', '/').'/')->create();
+    $this->base= $uri->using()
+      ->authority(new Authority($authority->host(), $authority->port()))
+      ->path(rtrim($uri->path() ?? '', '/').'/')
+      ->create()
+    ;
+
     $this->formats= $formats ?: Formats::defaults();
     $this->transfer= new Streamed($this);
     $this->marshalling= new Marshalling();
@@ -140,6 +145,8 @@ class Endpoint implements Traceable {
   public function open(RestRequest $request) {
     $target= $this->base->resolve($request->path());
     $conn= $this->connections->__invoke($target);
+    $conn->setConnectTimeout(10);
+    $conn->setTimeout(10);
     $headers= array_merge($this->headers, $request->headers());
 
     // RFC 6265: When the user agent generates an HTTP request, the user agent
