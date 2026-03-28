d65282be52c9a3ed2ef96e6240d1247c1c323507
Timm Friebe <thekid@thekid.de>
2026-03-28 15:37:50 +0100

    Add test for REST uploads

---
+++ src/test/php/webservices/rest/unittest/RestUploadTest.class.php
@@ -0,0 +1,61 @@
+<?php namespace webservices\rest\unittest;
+
+use io\streams\MemoryInputStream;
+use test\{Assert, Test};
+use webservices\rest\{Endpoint, RestRequest, RestUpload};
+
+class RestUploadTest {
+
+  /** Returns a new endpoint using the `TestConnection` class */
+  private function newEndpoint(string $base= 'http://test') {
+    return (new Endpoint($base, null, []))->connecting([TestConnection::class, 'new']);
+  }
+
+  #[Test]
+  public function can_create() {
+    new RestUpload($this->newEndpoint(), new RestRequest('POST', '/'));
+  }
+
+  #[Test]
+  public function pass_parameter() {
+    $upload= new RestUpload($this->newEndpoint(), new RestRequest('POST', '/'));
+    $upload->pass('name', 'Test');
+
+    Assert::equals(
+      "POST / HTTP/1.1\r\n".
+      "Connection: close\r\n".
+      "Host: test\r\n".
+      "Content-Type: multipart/form-data; boundary=---------------boundary1xp6132872336bc4\r\n".
+      "Transfer-Encoding: chunked\r\n".
+      "\r\n".
+      "-----------------boundary1xp6132872336bc4\r\n".
+      "Content-Disposition: form-data; name=\"name\"\r\n".
+      "\r\n".
+      "Test\r\n".
+      "-----------------boundary1xp6132872336bc4--\r\n",
+      $upload->finish()->content()
+    );
+  }
+
+  #[Test]
+  public function transfer_stream() {
+    $upload= new RestUpload($this->newEndpoint(), new RestRequest('POST', '/'));
+    $upload->transfer('file', new MemoryInputStream('Test'), 'test.txt');
+
+    Assert::equals(
+      "POST / HTTP/1.1\r\n".
+      "Connection: close\r\n".
+      "Host: test\r\n".
+      "Content-Type: multipart/form-data; boundary=---------------boundary1xp6132872336bc4\r\n".
+      "Transfer-Encoding: chunked\r\n".
+      "\r\n".
+      "-----------------boundary1xp6132872336bc4\r\n".
+      "Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n".
+      "Content-Type: text/plain\r\n".
+      "\r\n".
+      "Test\r\n".
+      "-----------------boundary1xp6132872336bc4--\r\n",
+      $upload->finish()->content()
+    );
+  }
+}
