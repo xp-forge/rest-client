159bc54e1aa8136c79370678a867f00170c26e20
Timm Friebe <thekid@thekid.de>
2026-03-28 15:42:10 +0100

    Add streaming test

--- src/test/php/webservices/rest/unittest/RestUploadTest.class.php
+++ src/test/php/webservices/rest/unittest/RestUploadTest.class.php
@@ -38,9 +38,34 @@ class RestUploadTest {
   }
 
   #[Test]
-  public function transfer_stream() {
+  public function transfer_file() {
     $upload= new RestUpload($this->newEndpoint(), new RestRequest('POST', '/'));
     $upload->transfer('file', new MemoryInputStream('Test'), 'test.txt');
+
+    Assert::equals(
+      "POST / HTTP/1.1\r\n".
+      "Connection: close\r\n".
+      "Host: test\r\n".
+      "Content-Type: multipart/form-data; boundary=---------------boundary1xp6132872336bc4\r\n".
+      "Transfer-Encoding: chunked\r\n".
+      "\r\n".
+      "-----------------boundary1xp6132872336bc4\r\n".
+      "Content-Disposition: form-data; name=\"file\"; filename=\"test.txt\"\r\n".
+      "Content-Type: text/plain\r\n".
+      "\r\n".
+      "Test\r\n".
+      "-----------------boundary1xp6132872336bc4--\r\n",
+      $upload->finish()->content()
+    );
+  }
+
+  #[Test]
+  public function stream_to_part() {
+    $upload= new RestUpload($this->newEndpoint(), new RestRequest('POST', '/'));
+    $part= $upload->stream('file', 'test.txt');
+    $part->write('Te');
+    $part->write('st');
+    $part->close();
 
     Assert::equals(
       "POST / HTTP/1.1\r\n".
